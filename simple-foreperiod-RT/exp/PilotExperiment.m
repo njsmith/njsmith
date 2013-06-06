ec3166918b121d948e6ceb34620310a996ef1f2e
njs@vorpus.org <njs@vorpus.org@9bffc044-a720-ec9b-77bd-87480a22d019>
2013-06-06 20:19:58 +0000

    move GetPoints to PilotExperiment.m, it's tiny so no point in cluttering things up like that

--- simple-foreperiod-RT/exp/PilotExperiment.m
+++ simple-foreperiod-RT/exp/PilotExperiment.m
@@ -276,5 +276,18 @@ toc;
 
     end
 
+    function points = GetPoints(respTime, trialType, params)
+
+        respTime = max(0, respTime - params.minRespTime);
+
+        points = (100/params.maxScoreTime) * max(0, params.maxScoreTime - respTime);
+
+        if strcmp(trialType, 'bonus')
+            points = params.bonusPoints * (respTime < params.cutoffTime);
+        end
+
+        points = round(points);
+    end
+
 end
 
