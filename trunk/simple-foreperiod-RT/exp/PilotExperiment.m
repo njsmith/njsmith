1598e025ad7944ccbfd99c2781ecf59bf5546cc1
njs@vorpus.org <njs@vorpus.org@9bffc044-a720-ec9b-77bd-87480a22d019>
2013-06-06 14:20:21 +0000

    more awesomeness

--- trunk/simple-foreperiod-RT/exp/PilotExperiment.m
+++ trunk/simple-foreperiod-RT/exp/PilotExperiment.m
@@ -14,17 +14,15 @@ params.minDelayTime  = 0.050;
 params.meanDelayTime = params.minDelayTime + params.nSig * params.stdDelayTime;
 params.maxDelayTime  = 2*params.minDelayTime + 2*params.nSig * params.stdDelayTime;
 
-
+params.scoreScreenDuration = 1.000;
 params.readyScreenDuration = 1.000;
 % If their RT is <50ms then they jumped the gun.
 params.minResponseTime = 0.050;
 
 trialsPerPractise = 3;
 trialsPerBlock = 3;
-numberOfBlocks = 5;
+numberOfBlocks = 2;
 wrapChars = 50;
-
-R = [];
 
 % points
 params.penaltyPoints = -1000;
@@ -33,6 +31,7 @@ params.penaltyTime = 5;
 params.minRespTime  = 0.075;
 params.maxScoreTime = 0.425;
 % speedy trials
+params.cutoffQuantile = 0.33;
 params.chanceOfSpeedyTrial = 0.25;
 params.cutoffTime = NaN; % to be set after practise trials
 params.bonusPoints = 500;
@@ -43,17 +42,18 @@ bkgrndGreyLevel = 100;
 % GetGamepadIndices.
 % To identify a gamepad: Gamepad('GetGamepadNamesFromIndices', indices)
 input_buttons = zeros(256, 1);
-if strcmp(computer, 'njs-laptop')
+if strcmp(computer, 'test')
     % Default -- keyboard
     % For some reason nothing else works :-( :-(
     % But hopefully on OS-X things will be better?
     input_device = [];
     % space bar
-    input_buttons(66) = 1;
-elseif strcmp(computer, 'lab')
-    error('need to check device settings for the lab');
-    % claim hard-RT access to 70% of CPU time (OS-X specific)
-    Priority(7);
+    input_buttons(kbname('space')) = 1;
+elseif strcmp(computer, 'lab-gamepad')
+    input_device = Gamepad('GetGamepadIndicesFromNames', 'Logitech Dual Action');
+    input_buttons([5, 6]) = 1;
+    Screen('Preference', 'DefaultFontSize', 36);
+    %Priority(9);
 else
     error('unknown computer');
 end
@@ -83,32 +83,35 @@ try
     
     disp('*starting experiment*');
     KbQueueStart(input_device);
+    results = [];
     
-    textScreen('Lets start with some practice! (XX)');
-    R = [R; RunBlock(r, 1, 1, 2, trialsPerPractise, 0)];
+    textScreen('Lets start with some practice! (XX)', 'wait');
+    results = [results RunBlock(r, 1, 1, 2, trialsPerPractise, 0)];
+    
     % calculate median response time
-    disp('trying to get median response time');
-    params.cutoffTime = nanmedian(R(1:end, end - 1));
+    disp('trying to get deadline response time');
+    params.cutoffTime = my_quantile(without_nan([results.resp_latency]), params.cutoffQuantile);
     params.cutoffTime
     % XX show instructions about bonus trials
-    textScreen('Now we''ll add bonus trials! (XX)');
-    R = [R; RunBlock(r, 1, 2, 2, trialsPerPractise, 1)];
-    dlmwrite(outpath, R, 'precision', '%6.3f');
-    textScreen('Any questions? Press any button when you''re ready to start the real experiment.');    
+    textScreen('Now we''ll add bonus trials! (XX)', 'wait');
+    results = [results RunBlock(r, 1, 2, 2, trialsPerPractise, 1)];
+    textScreen('Any questions? Press any button when you''re ready to start the real experiment.', 'wait');
     %% start experiment for real!
     for block = 1:numberOfBlocks
-        R = [R; RunBlock(r, 0, block, numberOfBlocks, trialsPerBlock, 1)];
-        dlmwrite(outpath, R, 'precision', '%6.3f');
+        results = [results RunBlock(r, 0, block, numberOfBlocks, trialsPerBlock, 1)];
+        save(outpath, '-v6', 'subjnum', 'results', 'params');
     end % end of a block
     %% thank you screen
     WaitSecs(0.1);
     Screen('DrawTexture',stimuliScrn,t_blank);
-    is_practise = (R(1:end, 1) == 1);
-    total_score = nansum(R(~is_practise, end));
+    non_practice_trials = [results.isPractice] == 0;
+    all_points = [results.points];
+    total_score = sum(all_points(non_practice_trials));
     DrawFormattedText(stimuliScrn, ['Thank you for taking part!\n ' int2str(total_score) ' points in total!!!'], 'center', 'center');
     Screen('Flip',stimuliScrn,0);
     KbQueueWait(input_device);
     fprintf('Total score: %i\n', total_score);
+    save(outpath, '-v6', 'subjnum', 'total_score', 'results', 'params');
     
     KbQueueRelease(input_device);
     Screen('CloseAll');
@@ -124,43 +127,66 @@ end
 
 Priority(0);
 toc;
-dlmwrite(outpath, R, 'precision', '%6.3f');
 
     function blockResults = RunBlock(r, isPractice, blockNum, totalBlocks, trialCount, includeBonus)
         blockResults = [];
         blockPoints = 0;
         for trial = 1:trialCount
-            if includeBonus && (rand(r) > params.chanceOfSpeedyTrial)
+            if includeBonus && (rand(r) < params.chanceOfSpeedyTrial)
                 trialType = 'bonus';
             else
                 trialType = 'regular';
             end
             trial_results = RunTrial(r, trialType, blockPoints);
-            blockPoints = blockPoints + trial_results(end);
-            blockResults = [blockResults; isPractice blockNum trial trial_results];
+            trial_results.isPractice = isPractice;
+            trial_results.blockNum = blockNum;
+            trial_results.trialInBlock = trial;
+            blockPoints = blockPoints + trial_results.points;
+            blockResults = [blockResults trial_results];
         end
-        disp('err')
         if isPractice
             pracString = ' practice';
         else
             pracString = '';
         end
-        disp('err2')
-        score = sum(blockResults(1:end, end));
-        disp(blockResults)
-        disp('err3')        
-        textScreen(sprintf('End of%s block %i (of %i). Score: %i\n\nTake a short break and stretch!', pracString, blockNum, totalBlocks, score));
+        score = sum([blockResults.points]);
+        textScreen(sprintf('End of%s block %i (of %i). Score: %i\n\nTake a short break and stretch!', pracString, blockNum, totalBlocks, score), 'wait');
     end
 
-    function textScreen(text)
+    function textScreen(text, how_long)
         KbQueueFlush(input_device);
         Screen('DrawTexture', stimuliScrn, t_blank);
-        DrawFormattedText(stimuliScrn, [text '\n\n\nPress button to continue.'], 'center', 'center');
+        if strcmp(how_long, 'wait')
+            extra_text = '\n\nPress button to continue.';
+        else
+            extra_text = '';
+        end
+        DrawFormattedText(stimuliScrn, [text extra_text], 'center', 'center');
         Screen('Flip', stimuliScrn);
-        KbQueueWait(input_device);
+        if strcmp(how_long, 'wait')
+            KbQueueWait(input_device);
+        else
+            WaitSecs(how_long);
+        end
     end
 
-    function trialData = RunTrial(r, trialType, blockPoints)
+    function m = my_nanmedian(x)
+        m = median(x(isfinite(x)));
+    end
+    function y = without_nan(x)
+        y = x(isfinite(x));
+    end
+    function p = my_quantile(x, q)
+        x = sort(x, 'ascend');
+        n = length(x);
+        k = round(n * q);
+        p = x(k);
+    end
+
+    function trialResults = RunTrial(r, trialType, blockPoints)
+        
+        trialResults.trialType = trialType;
+        
         if strcmp(trialType, 'bonus')
             Screen('DrawTexture',stimuliScrn,t_ready_bonus);
         elseif strcmp(trialType, 'regular')
@@ -171,17 +197,22 @@ dlmwrite(outpath, R, 'precision', '%6.3f');
         Screen('Flip', stimuliScrn, 0);
         WaitSecs(params.readyScreenDuration);
         
+        KbQueueFlush(input_device);
+        
         % now show blank screen with moving line until signal
         Screen('DrawTexture',stimuliScrn,t_blank);
-        delay = params.stdDelayTime * randn(r) + params.meanDelayTime;
-        delay = max(delay, params.minDelayTime);
-        delay = min(delay, params.maxDelayTime);
+        targdelay = params.stdDelayTime * randn(r) + params.meanDelayTime;
+        targdelay = max(targdelay, params.minDelayTime);
+        targdelay = min(targdelay, params.maxDelayTime);
+        trialResults.targdelay = targdelay;
         
-        [fp_vbl fp_stimon fp_flip fp_missed] = Screen('Flip', stimuliScrn);
+        [trialResults.foreperiodonset.vbl ...
+            trialResults.foreperiodonset.stimon ... 
+            trialResults.foreperiodonset.flip ...
+            trialResults.foreperiodonset.missed] = Screen('Flip', stimuliScrn);
         
-        yieldInterval = 0.002;
-        targetTime = fp_stimon + delay;
-        progressBarLimit = fp_stimon + params.maxDelayTime;
+        targetTime = trialResults.foreperiodonset.stimon + targdelay;
+        progressBarLimit = trialResults.foreperiodonset.stimon + params.maxDelayTime;
         jumped_gun = 0;
         response_time = NaN;
         while 1
@@ -229,8 +260,18 @@ dlmwrite(outpath, R, 'precision', '%6.3f');
             WaitSecs(params.penaltyTime);
         end
         % Show the end-of-trial click-to-continue screen
-        textScreen(sprintf('%i points!\n\nTotal this block: %i', points, blockPoints + points));
-        trialData = [double(trialType(1)), delay, fp_vbl, fp_stimon, fp_flip, fp_missed, stim_vbl, stim_stimon, stim_flip, stim_missed, response_time, RT, points];
+        textScreen(sprintf('%i points!\n\nTotal this block: %i', points, blockPoints + points), params.scoreScreenDuration);
+        
+        trialResults.go_onset.vbl = stim_vbl;
+        trialResults.go_onset.stimon = stim_stimon;
+        trialResults.go_onset.flip = stim_flip;
+        trialResults.go_onset.missed = stim_missed;
+        
+        trialResults.resp_time = response_time;
+        trialResults.resp_latency = RT;
+        trialResults.jumped_gun = jumped_gun;
+        trialResults.points = points;
+
     end
 
 end
